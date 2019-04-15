cd01b0291d87169abc194def29945b17b84fbfb0
Monica Qiu <m1102778586@gmail.com>
2019-04-15 00:39:38 -0400

    Added app icons and visuals for the society page

--- Pharos/SocietyViewController.swift
+++ Pharos/SocietyViewController.swift
@@ -6,8 +6,25 @@ import FirebaseStorage
 
 class SocietyViewController: UIViewController {
 
+    @IBOutlet weak var challengeButton: UIButton!
+    @IBOutlet weak var communityButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        challengeButton.layer.cornerRadius = 7
+        challengeButton.layer.shadowColor = UIColor.gray.cgColor
+        challengeButton.layer.shadowRadius = 10
+        challengeButton.layer.shadowOpacity = 1
+        challengeButton.layer.shadowOffset = CGSize(width: -10, height: 10)
+        
+        communityButton.layer.cornerRadius = 7
+        communityButton.layer.shadowColor = UIColor.red.cgColor
+        communityButton.layer.shadowRadius = 10
+        communityButton.layer.shadowOpacity = 1
+        communityButton.layer.shadowOffset = CGSize(width: -10, height: 10)
+        
     }
     
     @IBAction func logoutClicked(_ sender: Any) {
